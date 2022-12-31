a93a298fca51c055c82ca610ce15a7e6c22e34b8
Francis CLAIRICIA-ROSE-CLAIRE-JOSEPHINE <francis.clairicia-rose-claire-josephine@epitech.eu>
2022-12-31 16:01:32 +0100

    [UPDATE] Removed pre-commit install call in 'repo' command

--- commands/repo.py
+++ commands/repo.py
@@ -23,15 +23,12 @@ class RepoCommand(AbstractCommand):
     @classmethod
     def register_to_parser(cls, parser: ArgumentParser) -> None:
         parser.add_argument("-s", "--no-sync", dest="pip_sync", action="store_false", help="Do not run 'pip-sync'")
-        parser.add_argument(
-            "-p", "--no-pre-commit-install", dest="pre_commit", action="store_false", help="Do not run 'pre-commit install'"
-        )
 
     def run(self, args: Any, /) -> int:
-        self.setup(pip_sync=args.pip_sync, pre_commit_install=args.pre_commit)
+        self.setup(pip_sync=args.pip_sync)
         return 0
 
-    def setup(self, pip_sync: bool = True, pre_commit_install: bool = True) -> None:
+    def setup(self, pip_sync: bool = True) -> None:
         config: Configuration = self.config
 
         if config.venv_dir is not None:
@@ -40,6 +37,3 @@ class RepoCommand(AbstractCommand):
         PipCompileCommand(config).compile_all(REQUIREMENTS_FILES)
         if pip_sync:
             PipSyncCommand(config).sync()
-
-        if pre_commit_install:
-            self.exec_bin("pre-commit", "install")
